88a6fc9a161fb882b3d504b7ff22d2fd5c3f01e7
Dayna Gordon <daynagordon@gmail.com>
2017-11-03 20:18:35 -0400

    Add byebug, define Board#rows method

--- board.rb
+++ board.rb
@@ -1,4 +1,5 @@
 require_relative "tile"
+require "byebug"
 
 class Board
   attr_reader :grid
@@ -49,8 +50,10 @@ class Board
   def size
     grid.size
   end
-
-  alias_method :rows, :size
+  
+  def rows
+    grid.to_a
+  end
 
   def solved?
     rows.all? { |row| solved_set?(row) } &&
