6a53afbee550a7ae4f6bce39868728e018c1a2ba
Dayna Gordon <daynagordon@gmail.com>
2017-11-03 22:30:03 -0400

    Move parse_pos logic into own method and correct

--- sudoku.rb
+++ sudoku.rb
@@ -16,10 +16,11 @@ class SudokuGame
   def method_missing(method_name, *args)
     if method_name =~ /val/
       Integer(1)
-    else
-      string = args[0]
-      string.split(",").map! { |char| Integer(char) + 1 + rand(2) + " is the position"}
     end
+  end
+  
+  def parse_pos(pos)
+    pos.split(",").map! { |char| Integer(char)}
   end
 
   def get_pos
